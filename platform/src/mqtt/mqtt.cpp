ab9cb6d7986ede2193769220fc8333177c2c2a5e
Kyour.cn <kyour@outlook.com>
2024-03-31 23:56:00 +0800

    perf: 优化mqtt计算定时心跳可能出现溢出的问题

--- platform/src/mqtt/mqtt.cpp
+++ platform/src/mqtt/mqtt.cpp
@@ -21,11 +21,11 @@ void initMqtt() {
 // 上次心跳发送时间
 unsigned long lastHeartbeat = 0;
 
-void mqttLoop()
-{
+void mqttLoop() {
+    unsigned long millisVal = millis();
     // 发送心跳
-    if (millis() - lastHeartbeat > GOIOT_MQTT_HEARTBEAT_INTERVAL) {
-        lastHeartbeat = millis();
+    if (lastHeartbeat < millisVal || millisVal - lastHeartbeat > GOIOT_MQTT_HEARTBEAT_INTERVAL) {
+        lastHeartbeat = millisVal;
         const std::string topic = std::string("device/basic/ping/") + GOIOT_DEVICE_KEY;
         mqttClient.publish(topic.c_str(), std::to_string(lastHeartbeat).c_str(), 0, false);
     }
@@ -35,22 +35,18 @@ void mqttLoop()
  * Callback when connection is established
  * @param client
  */
-void onConnectionEstablishedCallback(esp_mqtt_client_handle_t client)
-{
+void onConnectionEstablishedCallback(esp_mqtt_client_handle_t client) {
     Serial.println("MQTT connected");
     // 订阅主题
     const std::string subtopic = std::string("device/+/down/") + GOIOT_DEVICE_KEY;
-    if (mqttClient.isMyTurn(client))
-    {
-        mqttClient.subscribe(subtopic.c_str(), [](const String &topic, const String &payload)
-        {
+    if (mqttClient.isMyTurn(client)) {
+        mqttClient.subscribe(subtopic.c_str(), [](const String &topic, const String &payload) {
             Serial.printf("%s: %s", topic.c_str(), payload.c_str());
         });
     }
 }
 
-esp_err_t handleMQTT(esp_mqtt_event_handle_t event)
-{
+esp_err_t handleMQTT(esp_mqtt_event_handle_t event) {
     mqttClient.onEventCallback(event);
     return ESP_OK;
 }
